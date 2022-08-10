ddb0c0537f98618a9804fd9ad20808305edccc64
Svetlana <bulatova44@gmail.com>
2022-08-11 01:59:00 +0300

    Created a function ExitFunction()

--- Task_41/Program.cs
+++ Task_41/Program.cs
@@ -3,8 +3,15 @@
 //0, 7, 8, -2, -2 -> 2
 //1, -7, -567, 89, 223-> 3
 
+
 int NumberValue()
 {
     int num = Convert.ToInt32(Console.ReadLine());
     return num;
+}
+
+
+void ExitFunction()
+{
+    string exitFun = " ";
 }
